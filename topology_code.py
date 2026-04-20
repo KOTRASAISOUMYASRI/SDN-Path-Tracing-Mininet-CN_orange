367aaea30772d197253f1bbfed98014b59f71a94
KOTRASAISOUMYASRI <kotrasaisoumyasri@gmail.com>
2026-04-20 12:53:18 +0530

    Create topology_code.py

---
+++ topology_code.py
@@ -0,0 +1,29 @@
+from mininet.topo import Topo
+
+class HybridTopo(Topo):
+
+    def build(self):
+
+        # Hosts
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+
+        # Switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+
+        # Main linear path
+        self.addLink(h1, s1)
+        self.addLink(s1, s2)
+        self.addLink(s2, s3)
+        self.addLink(s3, h2)
+
+        # Branch (extra host)
+        self.addLink(s2, h3)
+
+
+topos = {
+    'hybrid_topo': (lambda: HybridTopo())
+}
